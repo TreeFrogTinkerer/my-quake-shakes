c050b7f5e6652443e15b2c1101d2772970a82815
TreeFrogTinkerer <chris.kluener@gmail.com>
2026-09-05 18:33:16 -0700

    Update custom-actions.sh

--- custom-actions.sh
+++ custom-actions.sh
@@ -2,4 +2,4 @@
 
 # FTP to Home Assistant
 # Enable FTP server and a user with access to the the config share
-# curl -T my-quake-shakes.ics ftp://HA.IP.ADD.RESS/config/www/ --user username:password
+# curl -T ../my-quake-shakes.ics ftp://HA.IP.ADD.RESS/config/www/ --user username:password
